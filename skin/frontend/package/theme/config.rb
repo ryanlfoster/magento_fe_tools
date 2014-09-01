3a4ddff5e82f73971538c3695ee0e680c1bdb287
Igor Zimnitskiy <i.zimnitskiy@sysiq.com>
2014-09-01 15:45:23 +0300

    update compass config

--- skin/frontend/package/theme/config.rb
+++ skin/frontend/package/theme/config.rb
@@ -1,25 +1,17 @@
-require 'compass/import-once/activate'
-
-#--------------------
-# Configuration
-#--------------------
-
 http_path = "/"
 css_dir = "css"
 sass_dir = "scss"
 images_dir = "images"
 javascripts_dir = "js"
-environment = :production
-output_style = :compact
 line_comments = false
 
-# use LF line separator
+# environment = :production or :development
+environment = :production
+
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compact
+
+# LF Line Endings
 sass_options = {
     :unix_newlines => true
 }
-
-#--------------------
-
-# Note
-# output_style = :expanded or :nested or :compact or :compressed
-# environment = :production or :development

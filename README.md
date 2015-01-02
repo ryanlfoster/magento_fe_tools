0c2b6becfe73b8a6b206a066ad028d6e101ac0b4
Igor Zimnitskiy <i.zimnitskiy@gmail.com>
2015-01-02 16:06:42 +0100

    - update doc

--- README.md
+++ README.md
@@ -1,35 +1,27 @@
-**Magento FE Optimization Tools** - Basic magento theme, which contains useful tools for develop optimized projects.
+**Magento FE Toolskit** - contains gulp tasks and design patterns. Provide comfortable environment for front-end developers and help save time.
 
 #### Getting started
 Clone repository. 
 ```
 $ git clone https://github.com/IOZ/magento_fe_tools
 ```
-Copy folders **skin** and **app** to your Magento project.
-#### Download Grunt dependencies.
+Copy folders **skin** to your Magento project.
+#### Download Gulp dependencies.
 Open via terminal your theme
 ```
 $ cd skin/frontend/<package>/<theme>/
 ```
-load all plugin dependencies from **package.json**
+It will load all plugin dependencies from **package.json**
 ```
 $ npm install
 ```
-#### Run grunt task's
-All grunt tasks described at file
+#### Run gulp task's
+All gulp tasks described at file
 ```
-skin/frontend/<package>/<theme>/Gruntfile.js
+skin/frontend/<package>/<theme>/Gulpfile.js
 ```
 To run build process just type in terminal
 ```
-$ grunt
+$ gulp
 ```
-this command run default grunt task which invoke: sass watcher, autoprefixer, build all js plugin into one.
-
-Other grunt utils look at
-[Image optimization](https://github.com/IOZ/magento_fe_tools/wiki/Grunt-utilities#1-compress-images), [SVG optimization](https://github.com/IOZ/magento_fe_tools/wiki/Grunt-utilities#2-compress-svg-files).
-
-
-
-1. [SCSS config](https://github.com/IOZ/magento_fe_tools/wiki/Work-with-SCSS)
-2. [JSHint code quality tool](https://github.com/IOZ/magento_fe_tools/wiki/JSHint---code-quality-tool)
+this command run default gulp task.
--- skin/frontend/package/theme/Gulpfile.js
+++ skin/frontend/package/theme/Gulpfile.js
@@ -1,39 +1,62 @@
+/**
+ * Include plugins
+ */
 var gulp = require('gulp');
-var notify = require('gulp-notify');
-var imagemin = require('gulp-imagemin');
 var sass = require('gulp-ruby-sass');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
 var prefix = require('gulp-autoprefixer');
 var minifyCSS = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
 var header = require('gulp-header');
+var notify = require('gulp-notify');
 
 /**
  * Custom Configuration
  */
-var jsMap = [
-    'js/vendor/*.js',
-    'js/plugins/*.js',
-    'js/app/core.js',
-    'js/app/customPlugins/*.js',
-    'js/app/modules/*.js',
-    'js/app/main.js'
-];
+var paths = {
+    styles: {
+        src: 'scss/**/*.scss',
+        dest: 'css',
+        notify: 'CSS was compiled'
+    },
+    js: {
+        src: [
+            'js/vendor/*.js',
+            'js/plugins/*.js',
+            'js/app/core.js',
+            'js/app/customPlugins/*.js',
+            'js/app/modules/*.js',
+            'js/app/main.js'
+        ],
+        dest: 'js',
+        file: 'build.js',
+        suffix: '.min',
+        banner: false,
+        notify: 'JS was compressed'
+    },
+    images: {
+        src: 'img/original/**/*',
+        dest: 'img/build'
+    }
+};
 
-var jsBanner = [
-    '/**',
-    ' * Project name - Project description',
-    ' * @date - ${date}',
-    ' */\r\n'
-].join('\n');
+if (paths.js.banner) {
+    paths.js.banner = [
+        '/**',
+        ' * Project name - Project description',
+        ' * @date - ${date}',
+        ' */\r\n'
+    ].join('\n');
+}
 
 /**
  * Describe Gulp Tasks
  */
 
 gulp.task('sass', function() {
-    gulp.src('scss/**/*.scss')
+    gulp.src(paths.styles.src)
         .pipe(sass({
             style: 'compact',
             sourcemap: false,
@@ -41,36 +64,37 @@ gulp.task('sass', function() {
         }))
         .pipe(prefix({browsers: ['last 2 versions']}))
         .pipe(minifyCSS({keepBreaks:true}))
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(notify(paths.styles.notify));
 });
 
 gulp.task('js', function() {
-    gulp.src(jsMap)
-        .pipe(concat('build.js'))
-        .pipe(header(jsBanner, {date: new Date()}))
-        .pipe(gulp.dest('js'))
+    gulp.src(paths.js.src)
+        .pipe(concat(paths.js.file))
+        .pipe(header(paths.js.banner, {date: new Date()}))
+        .pipe(gulp.dest(paths.js.dest))
         .pipe(rename({
-            'suffix' : '.min'
+            'suffix' : paths.js.suffix
         }))
         .pipe(uglify())
-        .pipe(header(jsBanner, {date: new Date()}))
-        .pipe(gulp.dest('js'))
-        .pipe(notify('JS was compressed'))
+        .pipe(header(paths.js.banner, {date: new Date()}))
+        .pipe(gulp.dest(paths.js.dest))
+        .pipe(notify(paths.js.notify));
 });
 
 gulp.task('imagemin', function() {
-    gulp.src('img/original/**/*')
+    gulp.src(paths.images.src)
         .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true}))
-        .pipe(gulp.dest('img/build'));
+        .pipe(gulp.dest(paths.images.dest));
 });
 
 /**
  * Watch tasks
  */
 gulp.task('watch', function() {
-    gulp.watch('scss/**/*.scss', ['sass']);
-    gulp.watch(jsMap, ['js']);
-    gulp.watch('img/original/**/*', ['imagemin']);
+    gulp.watch(paths.styles.src, ['sass']);
+    gulp.watch(paths.js.src, ['js']);
+    gulp.watch(paths.images.src, ['imagemin']);
 });
 
 /**
